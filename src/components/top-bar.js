cd9f5dd67bce476e18a72e57fb25cef54a15c298
Matthew Pagan <mastapegs01@gmail.com>
2020-12-21 10:57:37 -0500

    refactor nav list

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -2,17 +2,39 @@ import { LitElement, html } from 'lit-element'
 
 class TopBar extends LitElement {
 
+  static get properties() {
+    return {
+      navList: { type: Array },
+    }
+  }
+
   createRenderRoot() {
     return this
+  }
+
+  constructor() {
+    super()
+    this.navList = [
+      {
+        path: '/',
+        text: 'Home',
+      },
+      {
+        path: '/about',
+        text: 'About',
+      },
+      {
+        path: '/contact',
+        text: 'Contact',
+      },
+    ]
   }
 
   render() {
     return html`
     <nav class='w-full h-12 bg-blue-700 text-white'>
       <ul class='flex h-full justify-evenly items-center'>
-        <li><a href='/'>Home</a></li>
-        <li><a href='/about'>About</a></li>
-        <li><a href='/contact'>Contact</a></li>
+        ${this.navList.map(({ path, text }) => html`<li><a href=${path}>${text}</a></li>`)}
       </ul>
     </nav>
     `
