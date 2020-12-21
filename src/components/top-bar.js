ccbeec024f8fd1a2eb7396bb229f2f5db02a6f43
Matthew Pagan <mastapegs01@gmail.com>
2020-12-21 10:46:33 -0500

    more tailwind for topbar

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -8,13 +8,13 @@ class TopBar extends LitElement {
 
   render() {
     return html`
-    <div class='w-full bg-blue-700 text-white'>
-      <ul class='flex'>
+    <nav class='w-full h-12 bg-blue-700 text-white'>
+      <ul class='flex h-full justify-evenly items-center'>
         <li><a href='/'>Home</a></li>
         <li><a href='/about'>About</a></li>
         <li><a href='/contact'>Contact</a></li>
       </ul>
-    </div>
+    </nav>
     `
   }
 
