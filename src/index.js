19bd2a5ade96fa36c56ba075b1ed1c4d44811329
Matthew Pagan <mastapegs01@gmail.com>
2020-12-20 23:03:13 -0500

    add tailwind/typography

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ class App extends LitElement {
   createRenderRoot() {
     return this
   }
-  
+
   render() {
     return html`
       <top-bar></top-bar>
-      <h1 class='shadow w-1/2 mx-auto px-10 py-4 mt-3 bg-white'>LitElement Web App</h1>
+      <h1 class='shadow-md max-w-md mx-auto px-10 py-4 my-3 bg-white'>LitElement Web App</h1>
+      <div class='container w-4/6 mx-auto p-6 bg-white rounded-lg shadow-lg'>
+        <div class='prose'>
+          <h2>Styling an entire block of Vanilla HTML with @tailwindcss/typography</h2>
+          <p>Right off the bat, check out how cool this code block looks...</p>
+          <pre><code>console.log('Hello, World!!!')</code></pre>
+          <p>That is amazing!</p>
+        </div>
+      </div>
     `
   }
 
