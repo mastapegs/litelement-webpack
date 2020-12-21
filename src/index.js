73d91b6530750fb1a6dcd44829a4c7feae0aae48
Matthew Pagan <mastapegs01@gmail.com>
2020-12-21 10:32:14 -0500

    export router component

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import { LitElement, html } from 'lit-element'
-import { Router } from '@vaadin/router'
 import "./styles.css"
 import './components/top-bar'
-import './pages/home-page'
-import './pages/about-page'
-import './pages/contact-page'
+import './components/page-router'
 
 class App extends LitElement {
-
   createRenderRoot() {
     return this
   }
-
-  firstUpdated() {
-    const outlet = document.getElementById('router-outlet')
-    const router = new Router(outlet)
-    router.setRoutes([
-      { path: '/', component: 'home-page' },
-      { path: '/about', component: 'about-page' },
-      { path: '/contact', component: 'contact-page' },
-    ])
-  }
-
   render() {
     return html`
       <top-bar></top-bar>
-      <div id='router-outlet'></div>
+      <page-router></page-router>
     `
   }
-
 }
 
 customElements.define('custom-app', App)
