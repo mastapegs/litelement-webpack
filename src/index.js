41d4af68666e435e29d95b066a09c72a3394cb87
Matthew Pagan <mastapegs01@gmail.com>
2020-12-20 23:20:22 -0500

    remove width from container

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ class App extends LitElement {
   render() {
     return html`
       <top-bar></top-bar>
-      <h1 class='shadow-md max-w-md mx-auto px-10 py-4 my-3 bg-white'>LitElement Web App</h1>
-      <div class='container w-4/6 mx-auto p-6 bg-white rounded-lg shadow-lg'>
+      <div class='shadow-md max-w-md mx-auto px-10 py-4 my-3 bg-white'>LitElement Web App</div>
+      <div class='container mx-auto p-6 bg-white rounded-lg shadow-lg'>
         <div class='prose'>
-          <h2>Styling an entire block of Vanilla HTML with @tailwindcss/typography</h2>
+          <h1>Styling an entire block of Vanilla HTML with Tailwind</h1>
           <p>Right off the bat, check out how cool this code block looks...</p>
           <pre><code>console.log('Hello, World!!!')</code></pre>
           <p>That is amazing!</p>
