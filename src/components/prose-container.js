cbe5fea44d86b90af73cfd9954d5452ae2e99ae4
Matthew Pagan <mastapegs01@gmail.com>
2020-12-21 21:03:01 -0500

    refactor prose-container to littailement

--- src/components/prose-container.js
+++ src/components/prose-container.js
@@ -1,18 +1,19 @@
-import { LitElement, html } from 'lit-element'
+import { html } from 'lit-element'
+import LitTailement from '../utils/LitTailement'
 
-class ProseContainer extends LitElement {
+class ProseContainer extends LitTailement {
+  
   static get properties() {
     return {
       component: { type: Object },
     }
   }
-  createRenderRoot() {
-    return this
-  }
+
   firstUpdated() {
     const title = document.getElementsByTagName('title')[0]
     title.innerText = 'Home | LitElement'
   }
+
   render() {
     return html`
     <div class='max-w-none prose prose-sm lg:prose-lg xl:prose-xl'>
@@ -20,6 +21,7 @@ class ProseContainer extends LitElement {
     </div>
     `
   }
+  
 }
 
 customElements.define('prose-container', ProseContainer)
