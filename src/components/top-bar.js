2b9edba7d19df2a6266bbc506fd116e0e255eb5b
Matthew Pagan <mastapegs01@gmail.com>
2020-12-24 00:44:17 -0500

    topbar css

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,7 +1,36 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement, html, css } from 'lit-element'
 
 class TopBar extends LitElement {
 
+  static get styles() {
+    return css`
+      nav {
+        background: blue;
+        color: white;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+      }
+      ul {
+        display: flex;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        justify-content: space-evenly;
+        padding: 1rem;
+      }
+      a {
+        display: inline-block;
+        background: hsla(0, 0%, 50%, 0.5);
+        padding: 0.5rem 1rem;
+        border-radius: 20%;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+        color: inherit;
+        text-decoration: inherit;
+        text-shadow: 1px 1px black;
+        font-weight: bold;
+      }
+    `
+  }
+  
   static get properties() {
     return {
       navList: { type: Array },
